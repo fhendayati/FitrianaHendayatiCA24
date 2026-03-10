bf8a9c0cc50690c74c82cc993586b672b6ae247d
Fitriana Hendayati SI CA 24 P <fhendayati@gmail.com>
2026-03-10 13:01:37 +0700

    update template

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
         <title>Perpustakaan - CI3 SB admin</title>
         <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet">
             <link href="<?= base_url('assets/css/sb-admin-2.min.css') ?>" rel="stylesheet">
-                <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+                <link href="<?= base_url('assets/vendor/bootstrap/scss/bootstrap.css') ?>" rel="stylesheet">
     </head>
     <body id="page-top">
         <div id="wrapper">
